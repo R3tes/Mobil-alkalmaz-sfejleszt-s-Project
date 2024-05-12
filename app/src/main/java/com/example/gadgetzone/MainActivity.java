c958ba00182ffaa88da2192380d3dfe651381299
baron <baron@inf.u-szeged.hu>
2024-05-12 13:43:18 +0200

    feat: added simple activity and stylings for application

--- app/src/main/java/com/example/gadgetzone/MainActivity.java
+++ app/src/main/java/com/example/gadgetzone/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.gadgetzone;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        username = findViewById(R.id.editTextUsername);
+        password = findViewById(R.id.editTextPassword);
+    }
+
+    public void login(View view) {
+        String usernameValue = username.getText().toString();
+        String passwordValue = password.getText().toString();
+
+        Log.i(LOG_TAG, "Username: " + usernameValue + " Password: " + passwordValue);
     }
 }
